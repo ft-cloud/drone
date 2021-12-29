ae2ac05e75ec1e363734e72cde01276b21ed3761
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-12-29 12:46:59 +0100

    Removed X-Powered-By

--- droneServer.js
+++ droneServer.js
@@ -29,7 +29,7 @@ client.connect().then(()=> {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
-
+app.disable('x-powered-by');
 
 app.use(cors());
 
